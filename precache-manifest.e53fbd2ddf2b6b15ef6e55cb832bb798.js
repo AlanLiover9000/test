3b83bdb9e47d0dcc8a5254bcee1db5fa049429bf
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-06-21 19:27:26 -0600

    first init

---
+++ precache-manifest.e53fbd2ddf2b6b15ef6e55cb832bb798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9218db93a075d07ef43a71fd4cecfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b09119f411e3164f0dbe",
+    "url": "./static/css/main.f6dd2aa4.chunk.css"
+  },
+  {
+    "revision": "06bf6628121bcf4e5c0c",
+    "url": "./static/js/2.16a16486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.16a16486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09119f411e3164f0dbe",
+    "url": "./static/js/main.ab2139f7.chunk.js"
+  },
+  {
+    "revision": "f6d81d4beeffce227069",
+    "url": "./static/js/runtime-main.deea157b.js"
+  }
+]);
